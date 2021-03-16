801de87b054035b783ac0ac059f32f8498916361
DJWickWack <35749854+DJWickWack@users.noreply.github.com>
2021-03-16 18:06:45 -0400

    Added navigation and more assest

---
+++ BlockCycles/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Levels(){
+        SceneManager.LoadScene("LevelSelect", LoadSceneMode.Single);
+    }
+
+    public void Help(){
+        SceneManager.LoadScene("Help", LoadSceneMode.Single);
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
