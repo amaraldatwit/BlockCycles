9459dc74eb594d18635e6dea2a5039fe27b34094
Dominic Amaral <amaralk2@wit.edu>
2021-04-05 13:14:48 -0400

    Added Grid rotation functionality

---
+++ BlockCycles/Assets/Scripts/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private int[,] grid;
+
+    public Grid(int w, int h){
+        this.width = w;
+        this.height = h;
+
+        grid = new int[w, h];
+    }
+}
+
