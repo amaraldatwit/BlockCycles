130bf08092b8617f84b1bc754bd3404604fc4c04
Dominic Amaral <65415383+amaraldatwit@users.noreply.github.com>
2021-04-06 17:04:51 -0400

    Changed level 1 canvas to world overlay

--- BlockCycles/Assets/Scripts/Grid.cs
+++ BlockCycles/Assets/Scripts/Grid.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid
 {
-    private int width;
-    private int height;
     private int[,] grid;
 
-    public Grid(int w, int h){
-        this.width = w;
-        this.height = h;
+    public Grid(){
 
-        grid = new int[w, h];
+        grid = new int[7, 7];
+        Debug.Log("Created Grid with height: " + 7 + " and width: " + 7 +" .");
+
+        for(int i = 0; i < grid.GetLength(0); i++){
+            for(int j = 0; j < grid.GetLength(1); j++){
+                grid[i, j] = 
+            }
+        }
     }
+
+    public
 }
 
