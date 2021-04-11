9452c260da1b403597d1151660ca4dcbe40a84be
Dominic Amaral <65415383+amaraldatwit@users.noreply.github.com>
2021-04-11 15:58:05 -0400

    Implemented static piece activation

--- BlockCycles/Assets/Scripts/StaticPiece.cs
+++ BlockCycles/Assets/Scripts/StaticPiece.cs
@@ -6,47 +6,37 @@ using UnityEngine.UI;
 public class StaticPiece : MonoBehaviour
 {
 
-    bool active;
+    int active_count;
     //public GameObject piece;
     public Sprite active_piece;
     public Sprite inactive_piece;
     // Start is called before the first frame update
     void Start()
     {
-        active = false;
-        //Sprite active_piece =  Resources.Load<Sprite>("Assets\\Board Assets\\BackTile_05");
-        //Sprite inactive_piece =  Resources.Load<Sprite>("Assets\\Board Assets\\BackTile_05_Red");
+        active_count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If sharing an edge with the grid, then become active
-        //Should static pieces blocks be considered a group, and any shared edge activates the group?
-        //If static piece collides with grid blocks then activate all children
-        //If static piece does not collide with grid blocks then deactivate all children
-    }
-
-    void OnTriggerEnter2D(Collider2D other){
-        Debug.Log("Collided with grid");
-        if(active == false){
-            for(int i = 0; i < this.gameObject.transform.childCount; i++){
-                SpriteRenderer s = this.gameObject.transform.GetChild(i).GetComponent<SpriteRenderer>();
-                s.sprite = active_piece;
-            }
-            active = true;
-        }
-    }
+    }    
 
     void OnTriggerExit2D(Collider2D other){
         Debug.Log("Collide over");
-        if(active == true){
-            for(int i = 0; i < this.gameObject.transform.childCount; i++){
-                Sprite s = this.gameObject.transform.GetChild(i).GetComponent<SpriteRenderer>().sprite;
-                s = inactive_piece;
+        active_count--;
+        if(active_count <= 0){
+                for(int i = 0; i < this.gameObject.transform.childCount; i++){
+                    this.gameObject.transform.GetChild(i).gameObject.GetComponent<Image>().sprite = inactive_piece;
+                }
             }
-            active = false;
-        }
     }
-
+    void OnTriggerEnter2D(Collider2D other){
+            Debug.Log("Collided with grid");
+            active_count++;
+            if(active_count > 0){
+                for(int i = 0; i < this.gameObject.transform.childCount; i++){
+                    this.gameObject.transform.GetChild(i).gameObject.GetComponent<Image>().sprite = active_piece;
+                }
+            }
+        }
 }
